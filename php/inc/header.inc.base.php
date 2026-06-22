91f0d73784b13d340918e89729b970c576db1d34
Lau <77939372+LauOJ@users.noreply.github.com>
2026-06-22 11:43:13 +0200

    Modernize header.inc.base.php and fix CSS cache buster
    - dirname(__DIR__, 2) instead of chained dirname calls
    - absolute path for header.inc.version.php require
    - reuse $language variable instead of calling get_session_language() twice
    - remove debug HTML comment from aixada_js_src output
    - aixada_custom_css now uses $aixada_vesion_lastDate (consistent with JS)
    - aixada_custom_css checks file exists before outputting link tag
    - add PHPDoc to all three functions

--- php/inc/header.inc.base.php
+++ php/inc/header.inc.base.php
@@ -1,43 +1,68 @@
-<?php 
+<?php
+
+/**
+ * Base initialization loaded by every Aixada page via header.inc.php.
+ * Loads config, utilities, language file, and defines JS/CSS helper functions.
+ *
+ * @package Aixada
+ */
+
 if (!defined('__ROOT__')) {
     define('DS', DIRECTORY_SEPARATOR);
-    define('__ROOT__', dirname(dirname(dirname(__FILE__))).DS); 
+    define('__ROOT__', dirname(__DIR__, 2) . DS);
 }
 
-require_once("header.inc.version.php"); // To obtain: $aixada_vesion_lastDate
-require_once(__ROOT__ . 'local_config'.DS.'config.php');
-require_once(__ROOT__ . 'php'.DS.'utilities'.DS.'general.php');
-require_once(__ROOT__ . 'php'.DS.'utilities'.DS.'negative_balances.php');
+require_once(__DIR__ . "/header.inc.version.php");
+require_once(__ROOT__ . 'local_config' . DS . 'config.php');
+require_once(__ROOT__ . 'php' . DS . 'utilities' . DS . 'general.php');
+require_once(__ROOT__ . 'php' . DS . 'utilities' . DS . 'negative_balances.php');
 
-$language = get_session_language(); 
+$language      = get_session_language();
 $default_theme = get_session_theme();
 
-require_once(__ROOT__ . 'local_config'.DS.'lang'.DS. get_session_language() . '.php');
+require_once(__ROOT__ . 'local_config' . DS . 'lang' . DS . $language . '.php');
 
-// To declare common JavaScript sources used by a Aixada page.
-function aixada_js_version() {
+/**
+ * Returns the cache-buster version string for JS and CSS assets.
+ *
+ * @return string Version date string (e.g. '20181210_140125').
+ */
+function aixada_js_version()
+{
     global $aixada_vesion_lastDate;
     return $aixada_vesion_lastDate;
 }
-function aixada_js_src($useMenus = true, $rootJs = '') {
+
+/**
+ * Returns HTML script tags for the core Aixada JavaScript files.
+ *
+ * @param bool   $useMenus Include menu JS files (set false for login/print pages).
+ * @param string $rootJs   Path prefix to the JS root, e.g. '../' for subpages.
+ * @return string HTML <script> tags ready to embed.
+ */
+function aixada_js_src($useMenus = true, $rootJs = '')
+{
     global $aixada_vesion_lastDate;
-    $src = "<!-- aixada_js_src rootJs='{$rootJs}' -->";
+    $v = $aixada_vesion_lastDate;
+
+    $src = '';
     if ($useMenus) {
         $src .= "
-        <script src=\"{$rootJs}js/fgmenu/fg.menu.js?v={$aixada_vesion_lastDate}\"></script>
-        <script src=\"{$rootJs}js/aixadautilities/jquery.aixadaMenu.js?v={$aixada_vesion_lastDate}\"></script>";
+        <script src=\"{$rootJs}js/fgmenu/fg.menu.js?v={$v}\"></script>
+        <script src=\"{$rootJs}js/aixadautilities/jquery.aixadaMenu.js?v={$v}\"></script>";
     }
     $src .= "
-        <script src=\"{$rootJs}js/aixadautilities/jquery.aixadaXML2HTML.js?v={$aixada_vesion_lastDate}\"></script>
-        <script src=\"{$rootJs}js/aixadautilities/jquery.aixadaUtilities.js?v={$aixada_vesion_lastDate}\"></script>
-        <script src=\"{$rootJs}js/aixadautilities/i18n/aixadaUtilities-" . get_session_language() . 
-            ".js?v={$aixada_vesion_lastDate}\"></script>";
+        <script src=\"{$rootJs}js/aixadautilities/jquery.aixadaXML2HTML.js?v={$v}\"></script>
+        <script src=\"{$rootJs}js/aixadautilities/jquery.aixadaUtilities.js?v={$v}\"></script>
+        <script src=\"{$rootJs}js/aixadautilities/i18n/aixadaUtilities-" . get_session_language() .
+            ".js?v={$v}\"></script>";
+
     if (get_config('use_ajaxQueue')) {
         $src .= "
-        <script src=\"{$rootJs}js/jquery-ajaxQueue/jQuery.ajaxQueue.js?v={$aixada_vesion_lastDate}\"></script>";
+        <script src=\"{$rootJs}js/jquery-ajaxQueue/jQuery.ajaxQueue.js?v={$v}\"></script>";
     } else {
         $src .= "
-        <script src=\"{$rootJs}js/jquery-ajaxQueue/jQuery.ajaxQueueNo.js?v={$aixada_vesion_lastDate}\"></script>";
+        <script src=\"{$rootJs}js/jquery-ajaxQueue/jQuery.ajaxQueueNo.js?v={$v}\"></script>";
     }
 
     $src .= include_negative_balances_js();
@@ -45,7 +70,18 @@ function aixada_js_src($useMenus = true, $rootJs = '') {
     return $src . "\n";
 }
 
-// Funció per incluir CSS personalitzat a totes les pàgines
-function aixada_custom_css() {
-    return '<link rel="stylesheet" type="text/css" media="screen" href="css/vinagreta-custom.css?v=4.4"/>' . "\n";
+/**
+ * Returns an HTML link tag for the coop-specific custom CSS, if it exists.
+ * Uses the same cache-buster version as JS assets.
+ *
+ * @return string HTML <link> tag, or empty string if no custom CSS file is present.
+ */
+function aixada_custom_css()
+{
+    global $aixada_vesion_lastDate;
+    $css_path = __ROOT__ . 'css' . DS . 'vinagreta-custom.css';
+    if (!file_exists($css_path)) {
+        return '';
+    }
+    return '<link rel="stylesheet" type="text/css" media="screen" href="css/vinagreta-custom.css?v=' . $aixada_vesion_lastDate . '"/>' . "\n";
 }
